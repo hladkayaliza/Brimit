e47f56dfe58e6e2c3b756d8bf9fa566eaa1005a5
Liza <liza.hladkaya@gmail.com>
2021-06-06 23:31:35 +0300

    Refactoring parseparseUrl

--- assets/scripts/parser.js
+++ assets/scripts/parser.js
@@ -1,14 +1,6 @@
 export function parseUrl(urlParameters) {
     if (urlParameters !== '') {
-        let model = {};
         let parameters = urlParameters.substring(1);
-        let parametersPairs = parameters.split('&');
-        parametersPairs.forEach(function (item) {
-            let pair = item.split('=');
-            if (pair[1] && !(pair[1] === 'null' || pair[1] === 'undefined')) {
-                model[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
-            }
-         });
-        return model;
+        return Object.fromEntries(new URLSearchParams(parameters));
     }
 }
--- assets/scripts/request-helper.js
+++ assets/scripts/request-helper.js
@@ -1,9 +1,9 @@
 import { URLS } from "./constants.js";
 
 export function generateRequest(object) {
-    let parameters = Object.keys(object).reduce(function(result, item) {
+    let parameters = (Object.keys(object).reduce(function(result, item) {
         return result.concat(`${decodeURIComponent(item)}=${decodeURIComponent(object[item])}&`);
-    },'');
+    },'')).slice(0, -1);
 
     return `${URLS.BASE}?${parameters}`;
 };
